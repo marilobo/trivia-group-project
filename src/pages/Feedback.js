8d303524bd665f11897be5d1f0f0270fa1942941
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-11-11 19:16:08 -0300

    realiza requisito 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import HeaderUser from '../components/HeaderUser';
 
 class Feedback extends Component {
+  playAgainButton = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <HeaderUser />
         <p data-testid="feedback-text">Feedback</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.playAgainButton }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Feedback;
