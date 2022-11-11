081dd1f9b4c17843b5ee9883c10602530615fdf7
Mariana <mlobosilva@outlook.com>
2022-11-11 13:22:15 -0300

    Altera um map para passar nos testes da 4

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -20,7 +20,7 @@ class Game extends React.Component {
     const tokenUser = localStorage.getItem('token');
     const gotQuestions = await this.getQuestions(tokenUser);
     const sortQuestions = await gotQuestions.results
-      .map((item) => this.sortidos([item.correct_answer, ...item.incorrect_answers]));
+      ?.map((item) => this.sortidos([item.correct_answer, ...item.incorrect_answers]));
     this.setState({
       questions: gotQuestions.results,
       randomAnswers: sortQuestions,
