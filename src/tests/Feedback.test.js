080aaed170567ab091dc30910b667b4c906a2bc4
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-11-12 19:10:46 -0300

    requisito 17 feito

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -3,7 +3,8 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import App from '../App.js';
 import { act } from 'react-dom/test-utils';
-import HeaderUser from '../components/HeaderUser';
+import Feedback from '../pages/Feedback.js';
+
 
 describe('Testa a page <Feedback />', () => {
   it('Testa se a página tem a rota correta', () => {
@@ -14,9 +15,58 @@ describe('Testa a page <Feedback />', () => {
   });
 
   it('Testa se existe um elemento onde é retornado o feedback', () => {
-    const { history } = renderWithRouterAndRedux(<App />);
-    act(() => history.push('/feedback'));
+    renderWithRouterAndRedux(<Feedback />);
     const feedbackText = screen.queryByTestId('feedback-text');
     expect(feedbackText).toBeInTheDocument();
+
+  });
+
+  it('Testa se existe um botão de jogar novamente', () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const btnPlayAgain = screen.queryByTestId('btn-play-again');
+    expect(btnPlayAgain).toBeInTheDocument();
+
+  });
+
+  it('Testa se ao clicar no botão de jogar novamente é redirecionado para página inicial', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => history.push('/feedback'));
+
+    const btnPlayAgain = screen.queryByTestId('btn-play-again');
+    userEvent.click(btnPlayAgain);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+  });
+
+  it('Testa se existe um botão para acessar o Ranking', () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const btnRankig = screen.queryByTestId('btn-ranking');
+    expect(btnRankig).toBeInTheDocument();
+  });
+
+  it('Testa se o botão para acessar o Rancking leva a outra página', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => history.push('/feedback'));
+
+    const btnRankig = screen.queryByTestId('btn-ranking');
+    userEvent.click(btnRankig);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/ranking');
+
+  });
+
+  it('Testa se os acertos aparecem na página', () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const assertions = screen.queryByTestId('feedback-total-question');
+    expect(assertions).toBeInTheDocument();
+  });
+
+  it('Testa se o Score aparece na página', () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const score = screen.queryByTestId('feedback-total-score');
+    expect(score).toBeInTheDocument();
   });
 });
