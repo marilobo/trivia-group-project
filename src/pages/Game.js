68a0529717f22024a1c8264434bafe87b9530ea6
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-11-17 19:38:31 -0300

    começa a estilizar o header

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -129,90 +129,93 @@ class Game extends React.Component {
     const n4 = 4;
     const { history } = this.props;
     return (
-      <div className="cssMesmo">
+      <>
         <HeaderUser />
-        <h1>Game</h1>
-        {
-          questions.map((item, index) => {
-            if (posi === index) {
-              return (
-                <div key={ item }>
-                  <div className="all-container">
-                    <div className="question-container">
-                      <h2
-                        data-testid="question-category"
-                        className="question-category"
-                      >
-                        {decode(item.category)}
-                      </h2>
-                      <h3
-                        data-testid="question-text"
-                        className="question-text"
-                      >
-                        {decode(item.question)}
-                      </h3>
-                      <span className="timer-container">
-                        <p className="timer">
-                          { timer === 0 ? 0
-                            : (
-                              <span>
-                                {timer}
-                                <img src={ hourglass } alt="time" />
-                              </span>) }
-                        </p>
-                      </span>
-                    </div>
-                    <div data-testid="answer-options" className="answer-options">
-                      {
-                        randomAnswers[index]
-                          .map((it, ind) => {
-                            const a = item.correct_answer === it ? 'green' : 'red';
-                            return (
-                              <button
-                                key={ it }
-                                data-testid={ item.correct_answer === it
-                                  ? 'correct-answer' : `wrong-answer-${ind}` }
-                                type="button"
-                                onClick={ () => this.handleClick(index, it) }
-                                disabled={ disabled }
-                                className={ corSimCorNao ? a : 'option' }
-                              >
-                                {decode(it)}
-                              </button>
-                            );
-                          })
-                      }
-                      {
-                        viewBtnNext && (
-                          <button
-                            type="button"
-                            data-testid="btn-next"
-                            onClick={ () => {
-                              this.setState({
-                                posi: posi + 1,
-                                viewBtnNext: false,
-                                corSimCorNao: false,
-                                disabled: false,
-                                timer: 30,
-                              });
-                              if (posi === n4) {
-                                history.push('/feedback');
-                              }
-                            } }
-                          >
-                            Next
-                          </button>
-                        )
-                      }
+        <div className="cssMesmo">
+          <h1>Game</h1>
+          {
+            questions.map((item, index) => {
+              if (posi === index) {
+                return (
+                  <div key={ item }>
+                    <div className="all-container">
+                      <div className="question-container">
+                        <h2
+                          data-testid="question-category"
+                          className="question-category"
+                        >
+                          {decode(item.category)}
+                        </h2>
+                        <h3
+                          data-testid="question-text"
+                          className="question-text"
+                        >
+                          {decode(item.question)}
+                        </h3>
+                        <span className="timer-container">
+                          <p className="timer">
+                            { timer === 0 ? 0
+                              : (
+                                <span>
+                                  {timer}
+                                  <img src={ hourglass } alt="time" />
+                                </span>) }
+                          </p>
+                        </span>
+                      </div>
+                      <div data-testid="answer-options" className="answer-options">
+                        {
+                          randomAnswers[index]
+                            .map((it, ind) => {
+                              const a = item.correct_answer === it ? 'green' : 'red';
+                              return (
+                                <button
+                                  key={ it }
+                                  data-testid={ item.correct_answer === it
+                                    ? 'correct-answer' : `wrong-answer-${ind}` }
+                                  type="button"
+                                  onClick={ () => this.handleClick(index, it) }
+                                  disabled={ disabled }
+                                  className={ corSimCorNao ? a : 'option' }
+                                >
+                                  {decode(it)}
+                                </button>
+                              );
+                            })
+                        }
+                        {
+                          viewBtnNext && (
+                            <button
+                              type="button"
+                              data-testid="btn-next"
+                              className="btn-next"
+                              onClick={ () => {
+                                this.setState({
+                                  posi: posi + 1,
+                                  viewBtnNext: false,
+                                  corSimCorNao: false,
+                                  disabled: false,
+                                  timer: 30,
+                                });
+                                if (posi === n4) {
+                                  history.push('/feedback');
+                                }
+                              } }
+                            >
+                              Next
+                            </button>
+                          )
+                        }
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-            return null;
-          })
-        }
-      </div>
+                );
+              }
+              return null;
+            })
+          }
+        </div>
+      </>
     );
   }
 }
