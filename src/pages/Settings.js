86bff38722ee8db997d6a1f99ee5bcfab8068aba
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-11-19 13:46:51 -0300

    estiliza a página settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getURL } from '../redux/actions/action';
+import { TiArrowBack } from 'react-icons/ti';
+import '../style/settings.css';
 
 const anyDifficulty = 'Any Difficulty';
 class Settings extends React.Component {
@@ -73,70 +75,82 @@ class Settings extends React.Component {
   render() {
     const { difficulties, types, category, difficulty, type, save } = this.state;
     return (
-      <div>
-        {console.log(save)}
-        <div>
-          <h1 data-testid="settings-title">Settings</h1>
-          <NavLink to="/" exact>Home</NavLink>
+      <>
+        <NavLink to="/" exact>
+          <TiArrowBack className="back" />
+        </NavLink>
+        <div className="ranking">
+          <div>
+            <h1
+              data-testid="settings-title"
+              className="ranking-title"
+            >
+              Settings
+            </h1>
+          </div>
+          <div className="settings-options">
+            <select
+              name="category"
+              value={ category }
+              onChange={ this.handleOptionValue }
+              className="options"
+            >
+              Category
+              <option value="Any Category">Any Category</option>
+              <option value="9">General Knowledge</option>
+              <option value="10">Entertainment: Books</option>
+              <option value="11">Entertainment: Film</option>
+              <option value="12">Entertainment: Music</option>
+              <option value="13">Entertainment: Musical & Theatres</option>
+              <option value="14">Entertainment: Television</option>
+              <option value="15">Entertainment: Video Games</option>
+              <option value="16">Entertainment: Board Games</option>
+              <option value="17">Science & Nature</option>
+              <option value="18">Science: Computers</option>
+              <option value="19">Science: Mathematics</option>
+              <option value="20">Mythology</option>
+              <option value="21">Sports</option>
+              <option value="22">Geography</option>
+              <option value="23">History</option>
+              <option value="24">Politics</option>
+              <option value="25">Art</option>
+              <option value="26">Celebrities</option>
+              <option value="27">Animals</option>
+              <option value="28">Vehicles</option>
+              <option value="29">Entertainment: Comics</option>
+              <option value="30">Science: Gadgets</option>
+              <option value="31">Entertainment: Japanese Anime & Manga</option>
+              <option value="32">Entertainment: Cartoon & Animations</option>
+            </select>
+            <select
+              name="difficulty"
+              value={ difficulty }
+              onChange={ this.handleOptionValue }
+              className="options"
+            >
+              Difficulty
+              { difficulties.map((item) => <option key={ item }>{ item }</option>) }
+            </select>
+            <select
+              name="type"
+              value={ type }
+              onChange={ this.handleOptionValue }
+              className="options"
+            >
+              Type
+              { types.map((item) => <option key={ item }>{ item }</option>) }
+            </select>
+          </div>
+          <button
+            type="button"
+            onClick={ this.saveChanges }
+            className="button-go-home"
+          >
+            Save Changes
+          </button>
+          <p>{ save }</p>
         </div>
-        <div>
-          <select
-            name="category"
-            value={ category }
-            onChange={ this.handleOptionValue }
-          >
-            Category
-            <option value="Any Category">Any Category</option>
-            <option value="9">General Knowledge</option>
-            <option value="10">Entertainment: Books</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="12">Entertainment: Music</option>
-            <option value="13">Entertainment: Musical & Theatres</option>
-            <option value="14">Entertainment: Television</option>
-            <option value="15">Entertainment: Video Games</option>
-            <option value="16">Entertainment: Board Games</option>
-            <option value="17">Science & Nature</option>
-            <option value="18">Science: Computers</option>
-            <option value="19">Science: Mathematics</option>
-            <option value="20">Mythology</option>
-            <option value="21">Sports</option>
-            <option value="22">Geography</option>
-            <option value="23">History</option>
-            <option value="24">Politics</option>
-            <option value="25">Art</option>
-            <option value="26">Celebrities</option>
-            <option value="27">Animals</option>
-            <option value="28">Vehicles</option>
-            <option value="29">Entertainment: Comics</option>
-            <option value="30">Science: Gadgets</option>
-            <option value="31">Entertainment: Japanese Anime & Manga</option>
-            <option value="32">Entertainment: Cartoon & Animations</option>
-          </select>
-          <select
-            name="difficulty"
-            value={ difficulty }
-            onChange={ this.handleOptionValue }
-          >
-            Difficulty
-            { difficulties.map((item) => <option key={ item }>{ item }</option>) }
-          </select>
-          <select
-            name="type"
-            value={ type }
-            onChange={ this.handleOptionValue }
-          >
-            Type
-            { types.map((item) => <option key={ item }>{ item }</option>) }
-          </select>
-        </div>
-        <button
-          type="button"
-          onClick={ this.saveChanges }
-        >
-          Save Changes
-        </button>
-        <p>{ save }</p>
-      </div>
+      </>
     );
   }
 }
